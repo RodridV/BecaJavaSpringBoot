03564ddd3df4888a13a8f0368f9ad1875e15ad5d
rodri <rodri@DESKTOP-IU8B4JD>
2021-09-30 18:38:07 +0200

    Conexion a la base de datos mediante localhost

--- Ej_CRUD_Spring_Boot/src/main/java/rodri/Aplicacion.java
+++ Ej_CRUD_Spring_Boot/src/main/java/rodri/Aplicacion.java
@@ -3,11 +3,12 @@ package rodri;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"controladores","modelos"})
 public class Aplicacion {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Aplicacion.class, args);
 	}
+	
 
 }
